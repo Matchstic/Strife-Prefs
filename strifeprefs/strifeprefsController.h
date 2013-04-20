4e7ab987a8437ed14f6bd63f87a1c468f248f1db
matchstick-dev <mclarke0805@gmail.com>
2013-04-20 15:59:38 +0100

    Initial Commit

---
+++ strifeprefs/strifeprefsController.h
@@ -0,0 +1,23 @@
+//
+//  strifeprefsController.h
+//  strifeprefs
+//
+//  Created by Matt Clarke on 20/04/2013.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Preferences/Preferences.h>
+
+@interface strifeprefsController : PSListController
+{
+}
+
+- (id)getValueForSpecifier:(PSSpecifier*)specifier;
+- (void)setValue:(id)value forSpecifier:(PSSpecifier*)specifier;
+- (void)followOnTwitter:(PSSpecifier*)specifier;
+- (void)visitWebSite:(PSSpecifier*)specifier;
+- (void)makeDonation:(PSSpecifier*)specifier;
+
+@end
